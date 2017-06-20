d73d40e9a2638b177293be1a3cfba7065249aae5
goelvivek2013 <sachin@idifysolutions.com>
2017-06-20 15:58:37 +0530

    Customization as per requirement

--- app/helpers/render_sortable_tree_helper.rb
+++ app/helpers/render_sortable_tree_helper.rb
@@ -16,61 +16,46 @@ module RenderSortableTreeHelper
         node = options[:node]
 
         "
-          <li data-node-id='#{ node.id }' class='the_sortable_tree-item'>
-            <div class='ptz_table w100p p5 the_sortable_tree-item_content'>
-              <div class='ptz_tr'>
-                <div class='ptz_td vam w30'>
-                  #{ handler }
-                </div>
-
-                <div class='ptz_td vam'>
-                  #{ show_link }
-                </div>
-
-                <div class='ptz_td vam br-off w10 pr5'>
-                  #{ controls }
-                </div>
-              </div>
+          <li data-node-id='#{ node.id }'>
+            <div class='item'>
+              <i class='handle'></i>
+              #{'<b class="expand minus">-</b>' unless children.blank?}
+              #{ show_link }
+              #{ controls }
             </div>
-
             #{ children }
           </li>
         "
       end
 
-      def handler
-        "<div class='the_sortable_tree-handler p5'>
-          <i class='fa fa-arrows fs16'></i>
-        </div>"
-      end
-
       def show_link
         node = options[:node]
         ns   = options[:namespace]
-        url = h.url_for(:controller => options[:klass].pluralize, :action => :show, :id => node)
+        url = h.url_for(:controller => 'admin/' + options[:klass].pluralize, :action => :show, :id => node)
         title_field = options[:title]
 
-        "<div class='fs15'>
-          #{ h.link_to(node.send(title_field), url) }
-        </div>"
+        "<h4>#{ h.link_to node.send(title_field), url,  target: '_edit_topic' }</h4>"
       end
-
       def controls
         node = options[:node]
-
-        edit_path = h.url_for(:controller => options[:klass].pluralize, :action => :edit, :id => node)
-        destroy_path = h.url_for(:controller => options[:klass].pluralize, :action => :destroy, :id => node)
+        edit_path = h.url_for(controller: 'admin/' + options[:klass].pluralize, action: :edit, id: node)
+        destroy_path = h.url_for(controller: options[:klass].pluralize, action: :destroy, id: node)
+        new_path = h.url_for(controller: options[:klass].pluralize, action: :new, topic: { parent_id: node })
+        new_evoke_path = h.url_for("/admin/topics/#{node.id}/evokes")
 
         "
-          <a href='#{ edit_path }'>
-            <i class='fa fa-edit fs20'></i>
-          </a>
+          <div class='controls'>
+            #{h.link_to '', '#', class: :new, data: { 'reveal-id': 'newTopic', parent_id: node[:topic_id] }}
+            #{h.link_to '', edit_path, class: :edit, target: '_edit_topic'}
+            #{h.link_to '', destroy_path, class: :delete, method: :delete, data: { confirm: 'Are you sure?' }}
+            #{h.link_to 'Evokes', new_evoke_path, class: :new_evoke, style: 'width: auto !important;', target: '_new_evoke'}
+          </div>
         "
       end
 
       def children
         unless options[:children].blank?
-          "<ol class='the_sortable_tree-nested_set'>#{ options[:children] }</ol>"
+          "<ol class='nested_set'>#{ options[:children] }</ol>"
         end
       end
 
